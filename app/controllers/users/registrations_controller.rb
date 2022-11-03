219ee579fb3e78319533c3c5fac2f64a3ddbcb00
yuki-hatsuharu <yuki_iyashikeyogoto@icloud.com>
2022-11-03 18:53:19 +0900

    ユーザー編集機能追加後rubocop実行

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,5 +1,4 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-
   before_action :configure_account_update_params, only: [:update]
 
   protected
@@ -7,13 +6,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def configure_account_update_params
     devise_parameter_sanitizer.permit(:account_update, keys: [:profile])
   end
-  
- 
 
   def update_resource(resource, params)
     resource.update_without_current_password(params)
   end
-
-
-
 end
