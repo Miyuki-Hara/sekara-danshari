1839f3215a75213f5001530850a2add2da15c601
yuki-hatsuharu <yuki_iyashikeyogoto@icloud.com>
2022-11-06 22:29:03 +0900

    user機能修正済み

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,7 @@ class ItemsController < ApplicationController
 
   before_action :set_item, only: [:show, :edit, :update, :destroy]
 
-  before_action :logged_in_user, only:[:edit, :update, :destroy]
+  before_action :logged_in_user, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
