3a2a2711a4c3d8a215364675a2c9eb7391621b2f
yuki-hatsuharu <yuki_iyashikeyogoto@icloud.com>
2022-11-13 22:04:47 +0900

    itemテスト日本語対応に修正
    userについてファイル名修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -37,47 +37,47 @@ RSpec.describe Item, type: :model do
       it 'userが紐付いていないと登録できない' do
         @item.user = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include('User must exist')
+        expect(@item.errors.full_messages).to include('Userを入力してください')
       end
       it 'imageが空だと登録できない' do
         @item.image = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("Image can't be blank")
+        expect(@item.errors.full_messages).to include('画像を入力してください')
       end
       it 'nameが空だと登録できない' do
         @item.name = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("Name can't be blank")
+        expect(@item.errors.full_messages).to include('名前を入力してください')
       end
       it 'categoryが空だと登録できない' do
         @item.category_id = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("Category can't be blank")
+        expect(@item.errors.full_messages).to include('カテゴリーを入力してください', 'カテゴリーを選択してください')
       end
       it 'categoryが---だと登録できない' do
         @item.category_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include('Category must be other than 1')
+        expect(@item.errors.full_messages).to include('カテゴリーを選択してください')
       end
       it 'let_go_wayが空だと登録できない' do
         @item.let_go_way_id = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("Let go way can't be blank")
+        expect(@item.errors.full_messages).to include('手離し方を入力してください', '手離し方を選択してください')
       end
       it 'let_go_wayが---だと登録できない' do
         @item.let_go_way_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include('Let go way must be other than 1')
+        expect(@item.errors.full_messages).to include('手離し方を選択してください')
       end
       it 'let_go_reasonが空だと登録できない' do
         @item.let_go_reason_id = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("Let go reason can't be blank")
+        expect(@item.errors.full_messages).to include('手離した理由を入力してください', '手離した理由を選択してください')
       end
       it 'let_go_reasonが---だと登録できない' do
         @item.let_go_reason_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include('Let go reason must be other than 1')
+        expect(@item.errors.full_messages).to include('手離した理由を選択してください')
       end
     end
   end
